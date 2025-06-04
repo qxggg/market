471b7aa8e60c605f60453ecb6f6bac6c494c2416
q778817400 <2569729045@qq.com>
2025-06-05 04:45:33 +0800

    6.5.2

--- untitled1-domain/src/main/java/org/example/domain/strategy/service/armory/StrategyArmory.java
+++ untitled1-domain/src/main/java/org/example/domain/strategy/service/armory/StrategyArmory.java
@@ -1,4 +1,48 @@
 package org.example.domain.strategy.service.armory;
 
-public class StrategyArmory {
+
+import org.example.domain.strategy.repository.IStrategyRepository;
+import org.example.domain.strategy.model.entity.StrategyAwardEntity;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StrategyArmory implements IStrategyArmory {
+
+    @Resource
+    private IStrategyRepository repository;
+
+    @Override
+    public void assembleLotteryStrategy(Long strategyId) {
+        //1.查询策略配置
+        List<StrategyAwardEntity> strategyAwardEntities = repository.strategyAwardEntities(strategyId);
+
+        //2.获得最小概率
+        BigDecimal minAwardRate = strategyAwardEntities.stream()
+                .map(StrategyAwardEntity::getAwardRate)
+                .min(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+
+        //3.获得概率总和
+        BigDecimal totalAwardRate = strategyAwardEntities.stream()
+                .map(StrategyAwardEntity::getAwardRate)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        //4. 用 1 % 0.0001 获得概率范围，百分位，千分位，万分位
+        BigDecimal rateRange = totalAwardRate.divide(minAwardRate, 0, RoundingMode.CEILING);
+
+        ArrayList<Object> strategyAwardSearchRateTable = new ArrayList<>(rateRange.intValue());
+        for (StrategyAwardEntity strategyAwardEntity : strategyAwardEntities) {
+            Integer awardId = strategyAwardEntity.getAwardId();
+            BigDecimal awardRate = strategyAwardEntity.getAwardRate();
+
+            for (int i = 0; i < rateRange.multiply(awardRate).setScale(0, RoundingMode.CEILING).intValue(); i++){
+                strategyAwardSearchRateTable.add(awardId);
+            }
+        }
+     }
+
 }
---
+++ untitled1-infrastructure/src/main/java/org/example/infrastructure/persistent/repository/StrategyRepository.java
@@ -0,0 +1,46 @@
+package org.example.infrastructure.persistent.repository;
+
+import org.example.domain.strategy.model.entity.StrategyAwardEntity;
+import org.example.domain.strategy.repository.IStrategyRepository;
+import org.example.infrastructure.persistent.dao.IAwardDao;
+import org.example.infrastructure.persistent.dao.IStrategyAwardDao;
+import org.example.infrastructure.persistent.po.Award;
+import org.example.infrastructure.persistent.po.StrategyAward;
+import org.example.infrastructure.persistent.redis.IRedisService;
+import org.example.types.common.Constants;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StrategyRepository implements IStrategyRepository {
+
+    @Resource
+    private IStrategyAwardDao strategyAwardDao;
+
+    @Resource
+    private IRedisService redisService;
+
+    @Override
+    public List<StrategyAwardEntity> strategyAwardEntities(Long strategyId) {
+        String cacheKey = Constants.RedisKey.STRATEGY_AWARD_KEY + strategyId;
+        List<StrategyAwardEntity> strategyAwardEntities = redisService.getValue(cacheKey);
+        if (null != strategyAwardEntities && !strategyAwardEntities.isEmpty()) return strategyAwardEntities;
+
+        List<StrategyAward> strategyAwards = strategyAwardDao.queryStrategyAwardListByStrategyId(strategyId);
+        strategyAwardEntities = new ArrayList<>(strategyAwards.size());
+        for (StrategyAward strategyAward : strategyAwards) {
+            StrategyAwardEntity strategyAwardEntity = StrategyAwardEntity.builder()
+                    .strategyId(strategyAward.getStrategyId())
+                    .awardId(strategyAward.getAwardId())
+                    .awardCount(strategyAward.getAwardCount())
+                    .awardCountSurplus(strategyAward.getAwardCountSurplus())
+                    .awardRate(strategyAward.getAwardRate())
+                    .build();
+            strategyAwardEntities.add(strategyAwardEntity);
+        }
+        redisService.setValue(cacheKey, strategyAwardEntities);
+        return strategyAwardEntities;
+    }
+}
