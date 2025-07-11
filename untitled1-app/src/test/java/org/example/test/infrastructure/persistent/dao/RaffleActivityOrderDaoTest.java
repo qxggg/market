22d70c310a98caa3f3c6065d5ec1b76bc42cc261
qxg <2569729045@qq.com>
2025-07-11 09:58:24 +0800

    finished20

---
+++ untitled1-app/src/test/java/org/example/test/infrastructure/persistent/dao/RaffleActivityOrderDaoTest.java
@@ -0,0 +1,41 @@
+package org.example.test.infrastructure.persistent.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.example.infrastructure.persistent.dao.IRaffleActivityOrderDao;
+import org.example.infrastructure.persistent.po.RaffleActivityOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RaffleActivityOrderDaoTest {
+
+    @Resource
+    private IRaffleActivityOrderDao raffleActivityOrderDao;
+
+    @Test
+    public void test_insert(){
+        RaffleActivityOrder raffleActivityOrder = new RaffleActivityOrder();
+        raffleActivityOrder.setUserId("xiaofuge");
+        raffleActivityOrder.setActivityId(100301L);
+        raffleActivityOrder.setActivityName("测试活动");
+        raffleActivityOrder.setStrategyId(100006L);
+        raffleActivityOrder.setOrderId(RandomStringUtils.randomNumeric(12));
+        raffleActivityOrder.setOrderTime(new Date());
+        raffleActivityOrder.setState("not_used");
+        // 插入数据
+        raffleActivityOrderDao.insert(raffleActivityOrder);
+    }
+
+    @Test
+    public void query(){
+        System.out.println(raffleActivityOrderDao.queryRaffleActivityOrderByUserId("xiaofuge"));
+    }
+}
---
+++ untitled1-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,12 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.persistent.po.RaffleActivity;
+
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
+
